70b2f7c72032b56dd9edd34b7bccebf8ab6375d3
Kavya Sri <margamkavya>
2018-05-11 12:56:41 -0500

    intial code

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+
+
+export default function() {
+    return[
+       {title: "javaScript : the good Parts"},
+       {title: "Harry Potter"},
+       {title: "the Dark Tower"},
+       {title: "Eloquent Ruby"}
+    ]
+}
